4a3b7272ec3e85f42700d6535026b8d6a7971dbb
Marco Giacomuzzi <marco.giacomuzzi@gmail.com>
2019-03-08 08:51:07 +0100

    reto

---
+++ main.js
@@ -0,0 +1,242 @@
+const contacts = [
+    {
+      "name": "Piedad",
+      "surname": "Dunfee",
+      "email": "piedad.dunfee@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Elsie",
+      "surname": "Calvery",
+      "email": "elsie.calvery@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Rodger",
+      "surname": "Pye",
+      "email": "rodger.pye@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Mohammad",
+      "surname": "Pye",
+      "email": "mohammad.pacheo@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Emmie",
+      "surname": "Derryberry",
+      "email": "emmie.derryberry@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Lino",
+      "surname": "Kottwitz",
+      "email": "lino.kottwitz@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Brittney",
+      "surname": "Ruddock",
+      "email": "brittney.ruddock@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "familia"
+    },
+    {
+      "name": "Jaimie",
+      "surname": "Human",
+      "email": "jaimie.human@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Darleen",
+      "surname": "Kesten",
+      "email": "darleen.kesten@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Georgie",
+      "surname": "Hannold",
+      "email": "georgie.hannold@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "familia"
+    },
+    {
+      "name": "Doreatha",
+      "surname": "Wheeler",
+      "email": "doreatha.wheeler@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Christie",
+      "surname": "Solum",
+      "email": "christie.solum@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Carlita",
+      "surname": "Whitmire",
+      "email": "carlita.whitmire@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "familia"
+    },
+    {
+      "name": "Luciana",
+      "surname": "Jobe",
+      "email": "luciana.jobe@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Sade",
+      "surname": "Selig",
+      "email": "sade.selig@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Devon",
+      "surname": "Goodridge",
+      "email": "devon.goodridge@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Bruna",
+      "surname": "Starkes",
+      "email": "bruna.starkes@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Glynis",
+      "surname": "Hudnall",
+      "email": "glynis.hudnall@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Anissa",
+      "surname": "Leggett",
+      "email": "anissa.leggett@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "familia"
+    },
+    {
+      "name": "Moshe",
+      "surname": "Moxley",
+      "email": "moshe.moxley@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Shannon",
+      "surname": "Olivieri",
+      "email": "shannon.olivieri@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Janel",
+      "surname": "Vandever",
+      "email": "janel.vandever@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "favoritos"
+    },
+    {
+      "name": "Tashia",
+      "surname": "Fontes",
+      "email": "tashia.fontes@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Merrilee",
+      "surname": "Lynde",
+      "email": "merrilee.lynde@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Coral",
+      "surname": "Eastin",
+      "email": "coral.eastin@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Shandi",
+      "surname": "Hunsaker",
+      "email": "shandi.hunsaker@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    },
+    {
+      "name": "Wilburn",
+      "surname": "Meyer",
+      "email": "wilburn.meyer@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Quintin",
+      "surname": "Schrom",
+      "email": "quintin.schrom@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Claribel",
+      "surname": "Sipes",
+      "email": "claribel.sipes@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "trabajo"
+    },
+    {
+      "name": "Nevada",
+      "surname": "Chappelle",
+      "email": "nevada.chappelle@domain.com",
+      "country": "españa",
+      "phone": "607 123 456",
+      "group": "amigos"
+    }
+  ]
